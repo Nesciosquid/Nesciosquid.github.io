f22b5f076046d71743d64ba7cc7a254f7122077c
Aaron W. Heuckroth <a.heuckroth@gmail.com>
2013-07-11 18:51:22 -0400

    Refactoed index for js load of banner image.

---
+++ assets/js/derp2.js
@@ -0,0 +1,42 @@
+$(function(){
+
+	function setSize(style) {
+		var winwid = $(window).width();
+		var banner_class;
+		if (winwid > 980) {
+			banner_class = "d-" + style;
+			$('.heuck-banner').removeClass().addClass("heuck-banner hero-unit banner-desktop " + banner_class);
+		}
+
+		if (winwid < 980 && winwid >= 768) {
+			banner_class = "t-" + style;
+			$('.heuck-banner').removeClass().addClass("heuck-banner hero-unit banner-tablet " + banner_class);
+		}
+
+		if (winwid < 768) {
+			banner_class = "p-" + style;
+			$('.heuck-banner').removeClass().addClass("heuck-banner banner-phone p-fist " + banner_class);
+		}
+	}
+
+	var type;
+	var number =Math.round(Math.random() * 1);
+	if (number == 0)
+	{
+		type = "fist";
+	}
+
+	if (number == 1) 
+	{
+		type = "wet";
+	}
+
+	alert(number + type);
+
+
+	setSize(type);
+
+	$(window).resize(function() {
+		setSize(type);
+	});
+});
