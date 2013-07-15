a6389279be06de1f968e0b9b2bc0eb6d0e7d7192
Aaron W. Heuckroth <a.heuckroth@gmail.com>
2013-07-15 12:06:19 -0400

    Altered banner response behavior, began pagination for blog, restructured site.

---
+++ _site/assets/js/banner_response.js
@@ -0,0 +1,42 @@
+$(function(){
+
+	function setSize(style) {
+		var winwid = $(window).width();
+		var banner_class;
+		if (winwid > 980) {
+			banner_class = "d-" + style;
+			$('.heuck-banner').removeClass().addClass("heuck-banner hero-unit banner-desktop");
+			$('.heuck-banner').addClass(banner_class);
+		}
+
+		if (winwid < 980 && winwid >= 768) {
+			banner_class = "t-" + style;
+			$('.heuck-banner').removeClass().addClass("heuck-banner hero-unit banner-tablet ");
+			$('.heuck-banner').addClass(banner_class);
+		}
+
+		if (winwid < 768) {
+			banner_class = "p-" + style;
+			$('.heuck-banner').removeClass().addClass("heuck-banner banner-phone p-fist ");
+			$('.heuck-banner').addClass(banner_class);
+		}
+	}
+
+	var type;
+	var number =Math.round(Math.random() * 1);
+	if (number == 0)
+	{
+		type = "fist";
+	}
+
+	if (number == 1) 
+	{
+		type = "wet";
+	}
+	
+	setSize(type);
+
+	$(window).resize(function() {
+		setSize(type);
+	});
+});
--- assets/js/banner_response.js
+++ assets/js/banner_response.js
@@ -5,17 +5,20 @@ $(function(){
 		var banner_class;
 		if (winwid > 980) {
 			banner_class = "d-" + style;
-			$('.heuck-banner').removeClass().addClass("heuck-banner hero-unit banner-desktop " + banner_class);
+			$('.heuck-banner').removeClass().addClass("heuck-banner hero-unit banner-desktop");
+			$('.heuck-banner').addClass(banner_class);
 		}
 
 		if (winwid < 980 && winwid >= 768) {
 			banner_class = "t-" + style;
-			$('.heuck-banner').removeClass().addClass("heuck-banner hero-unit banner-tablet " + banner_class);
+			$('.heuck-banner').removeClass().addClass("heuck-banner hero-unit banner-tablet ");
+			$('.heuck-banner').addClass(banner_class);
 		}
 
 		if (winwid < 768) {
 			banner_class = "p-" + style;
-			$('.heuck-banner').removeClass().addClass("heuck-banner banner-phone p-fist " + banner_class);
+			$('.heuck-banner').removeClass().addClass("heuck-banner banner-phone p-fist ");
+			$('.heuck-banner').addClass(banner_class);
 		}
 	}
 
